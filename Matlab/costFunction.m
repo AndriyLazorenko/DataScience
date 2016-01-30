31a568df9d4b0213a035cf41eb51526fadd5a087
Andriy Lazorenko <andriy.lazorenko@gmail.com>
2016-01-30 17:53:07 +0200

    Delete costFunction.m

--- Matlab/costFunction.m
+++
@@ -1,7 +0,0 @@
-function J = costFunction (X,y, theta)
-%Cost function for linear regression
-m = length(y); %number of training examples
-predict = X*theta; % vectorized predicted values
-errorsSqrd = (predict-y).^2; %squared errors
-J = 1/(2*m)*sum(errorsSqrd); %cost function
-end
